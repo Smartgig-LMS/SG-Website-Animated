de7cfc5a0ea8a076b36bd68ff757ca72d4024f5a
Bhushan Patil <patilbhushan1190@gmail.com>
2023-11-27 18:07:58 +0530

    blog page added

--- src/Components/LandingPage/BlogFolder/PharmaBlog1.js
+++ src/Components/LandingPage/BlogFolder/PharmaBlog1.js
@@ -332,7 +332,7 @@ export default function PharmaBlog1() {
 
         <div className="d-flex justify-content-center align-items-center">
           <div className="p-5">
-            <h5 className="flex mt-5 mb-5" style={{ fontStyle: "italic" }}>
+            <h5 className="flex mt-5 mb-5" style={{ fontStyle: "italic",color:"#F38313" }}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="25"
@@ -343,7 +343,7 @@ export default function PharmaBlog1() {
               >
                 <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
               </svg>
-              How to Choose the right GxP solutions if you are a Pharma Company:
+              How to Choose the right GxP solutions if you are a Pharma Company
             </h5>
             <p>
               <h6 className="mt-4">
